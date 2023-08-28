d0205fe741683624bf67cb90b8f1992c78242b76
TJ <tj.gorman@compsys.com>
2023-08-28 10:11:30 -0500

    Make sure to check if uploaded file is executable

--- analyze.go
+++ analyze.go
@@ -7,7 +7,45 @@ import (
 	"strings"
 )
 
+func hasmz(targetPath string) bool {
+	// Open the file
+	file, err := os.Open(targetPath)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer file.Close()
+
+	// Read the first two bytes of the file
+	var header [2]byte
+	_, err = file.Read(header[:])
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+
+	// Check if the first two bytes are equal to "MZ"
+	// MZ is the magic header for PE, DLL, and DOS files
+	if string(header[:]) != "MZ" {
+		return false
+	}
+
+	return true
+}
+
 func analyzedump(targetPath string) string {
+
+	// Check if the file exists
+	if _, err := os.Stat(targetPath); err != nil {
+		fmt.Println(err.Error())
+		return "Error while analyzing file"
+	}
+
+	//Check if the file is a PE file to prevent exploitation
+	if hasmz(targetPath) {
+		return "Error while analyzing file (File is a PE file)"
+	}
+
 	app := "cdb"
 	arg0 := "-z"
 	arg1 := targetPath
@@ -18,7 +56,7 @@ func analyzedump(targetPath string) string {
 	stdout, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err.Error())
-		return ""
+		return "Error while launching debugger agent (Is this a valid dump file?)"
 	}
 
 	leftpoint := strings.Index(string(stdout), "*******************************************************************************")
